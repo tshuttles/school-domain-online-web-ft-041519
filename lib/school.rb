e337a962e98495f157a78325187cbf99881b5b20
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-23 15:31:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,9 @@ class School
   def add_student(name, grade)
     @roster = {grade => [name]}
     if @roster[grade] == [] 
-      @roster = {[grade] => [name]}
-    else 
+      @roster = {grade => [name]}
+    elsif @roster [grade] != [] 
+      @roster[grade] << [name]
     end 
   end 
 
